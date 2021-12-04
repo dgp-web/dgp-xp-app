ed01ab771b480fbfa788d2fcd0fe5de69fc5e38e
dgp-web <dgp-web@outlook.de>
2021-12-04 10:06:08 +0100

    Rename platformExpress to platformServer

--- projects/dgp-xp-app/src/lib/functions/platform-server.function.ts
+++ projects/dgp-xp-app/src/lib/functions/platform-server.function.ts
@@ -1,7 +1,7 @@
 import { Provider } from "injection-js";
 import { additionalProviders, bootstrapModule } from "./bootstrap-module.function";
 
-export function platformExpress(providers?: ReadonlyArray<Provider>) {
+export function platformServer(providers?: ReadonlyArray<Provider>) {
 
     if (providers) {
         providers.forEach(x => additionalProviders.push(x));
